10abd9ec6b4c5a1e9b12ddc65b2bc8bf83f65764
Artsem Stsepanenka <asstep@mail.ru>
2022-12-26 20:24:58 +0300

    Add Scroll

--- app/src/main/java/com/example/testtask/compose/HistoryItem.kt
+++ app/src/main/java/com/example/testtask/compose/HistoryItem.kt
@@ -1,13 +1,16 @@
 package com.example.testtask.compose
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -18,10 +21,11 @@ fun HistoryItem(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(modifier = Modifier,
-            style = TextStyle(fontSize = 25.sp),
-            text = number.toString(),
-
+        Text(text = number.toString(), fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(width = 1.dp, color = Color.Black)
+                .padding(16.dp)
         )
     }
 
--- app/src/main/java/com/example/testtask/compose/HistoryScreen.kt
+++ app/src/main/java/com/example/testtask/compose/HistoryScreen.kt
@@ -1,9 +1,17 @@
 package com.example.testtask.compose
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LiveData
 import com.example.testtask.room.model.NumberBin
 
@@ -12,8 +20,14 @@ fun HistoryScreen(
     list: LiveData<List<NumberBin>>
 ) {
     val list2 by list.observeAsState()
-    Column {
-
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .border(width = 2.dp, color = Color.Green)
+            .padding(16.dp)
+    )
+    {
             list2?.forEach { value ->
                 HistoryItem(value.bin)
         }
